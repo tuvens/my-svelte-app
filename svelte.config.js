a3f6959f1d651135fd28584e7cb322c7ba062bab
Ciarán Carroll <ciarancarroll@MacBook-Air.station>
2025-01-08 11:46:19 +0000

    Update Vercel adapter configuration and dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
-import adapter from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// Use the Vercel adapter and specify the Node.js runtime
-		adapter: vercel({
-			runtime: 'nodejs18.x'
-		})
+		// Use the Vercel adapter
+		adapter: vercel()
 	}
 };
 
