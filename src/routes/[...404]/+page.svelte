097380fc43dd5c5bdc8207715cf4fbc061a93ffc
tuvens <contact@freelanceweb.ie>
2025-01-09 14:56:57 +0000

    testing deployments to vercel

--- src/routes/[...404]/+page.svelte
+++ src/routes/[...404]/+page.svelte
@@ -2,6 +2,6 @@
     export let params;
   </script>
   
-  <h1>404 - Page Not Found</h1>
+  <h1>404 - Page Not Found Found?</h1>
   <p>The page you’re looking for doesn’t exist.</p>
   <a href="/">Go back home</a>
